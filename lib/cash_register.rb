871fb3d3ebcdeff8b369f5e717cddce68b5c1a78
Claudia Pierson <c172@umbc.edu>
2020-04-26 23:18:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,10 +4,7 @@ def initialize(total = 0)
   @total = total
 end
 
-# def discount
-#   @total.to_i
-# end
-# def total(total=100)
-#  s@total = total
-#  end
+ def discount
+   @total.to_i
 end
+end 
