842e29d6d45d3503b7f617a083d90789a71bae89
Claudia Pierson <c172@umbc.edu>
2020-04-26 23:18:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 class CashRegister
- attr_accessor :total, :discount 
+ attr_accessor :total, :discount
 def initialize(total = 0)
   @total = total
 end
 
-def discount
-  @total.to_i
-end
+# def discount
+#   @total.to_i
+# end
 # def total(total=100)
 #  s@total = total
 #  end
